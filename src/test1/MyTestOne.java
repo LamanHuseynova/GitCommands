d88fb19bdd66070a2adf2acb75dba154572ee984
lemanhuseynova <laman_hv@yahoo.com>
2022-05-02 19:33:49 -0500

    my commit

---
+++ src/test1/MyTestOne.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class MyTestOne {
+
+    public static void main(String[] args) {
+
+        System.out.println("My Test One");
+    }
+}
---
+++ src/test2/MyTestTwo.java
@@ -0,0 +1,8 @@
+package test2;
+
+public class MyTestTwo {
+
+    public static void main(String[] args) {
+        System.out.println("My Test Two");
+    }
+}
